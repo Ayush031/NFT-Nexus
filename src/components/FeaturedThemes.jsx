ce14a726d9cbcd154882519521f2a1059332f149
Ayush Kansal <ayushkansal303@gmail.com>
2024-03-14 04:40:53 +0530

    removed heading

--- src/components/FeaturedThemes.jsx
+++ src/components/FeaturedThemes.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function FeaturedThemes() {
     return (
         <>
-            <div className="text-5xl text-center m-5 font-bold text-transparent  bg-clip-text bg-gradient-to-r from-purple-400 to-orange-500">Featured Themes</div>
+            {/* <div className="text-5xl text-center m-5 font-bold text-transparent  bg-clip-text bg-gradient-to-r from-purple-400 to-orange-500">Featured Themes</div> */}
 
             <div className="relative overflow-hidden mb-20 ">
                 <div className="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
