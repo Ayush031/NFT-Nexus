087b2fc970b4fdaad404ef1ae0dcc05457212757
Ayush Kansal <ayushkansal303@gmail.com>
2024-03-14 10:06:14 +0530

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ function Footer() {
   return (
     <>
 
-      <footer className=" mt-5 footer footer-center p-4 bg-base-300 text-base-content">
+      <footer className="mt-5 footer footer-center p-4 bg-base-300 text-base-content">
         <aside>
-          <p>Copyright © 2024 - All right reserved by Nft Nexus</p>
+          <p>Copyright © 2024 - All right reserved by NFT Nexus</p>
         </aside>
       </footer>
     </>
